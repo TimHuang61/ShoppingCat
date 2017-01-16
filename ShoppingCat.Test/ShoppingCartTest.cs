2be6e627fc2585a8e3d4dc0084a8ad2878545b68
hasing0601@gmail.com <hasing0601@gmail.com>
2017-01-17 01:01:13 +0800

    Add project files.

---
+++ ShoppingCat.Test/ShoppingCartTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCat.Test
+{
+    [TestFixture]
+    public class ShoppingCartTest
+    {
+        [Test]
+        public void Buy_The_First_Book_Should_100()
+        {
+            Assert.Fail();
+        }
+    }
+}
